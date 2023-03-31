c3664e14c8438156b8d6362a2ce38d0458d26b6a
pᴉʞ ɥsɐɹʇ ɹnoʎ <91683715+Your-Trash-kid@users.noreply.github.com>
2023-03-31 09:33:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.youtube.com/channel/UCgkRkvD3IiZCtaxjyG-mwSA")
+driver.get("https://google.com")
